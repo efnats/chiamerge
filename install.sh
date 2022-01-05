f0c14d865d5abd1febcf6afe36e531bdd8012ec0
efnats <22054801+efnats@users.noreply.github.com>
2022-01-06 00:20:08 +0100

    add support for ntfs in installer and better explanation of whats going on

--- install.sh
+++ install.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 echo ""
-echo "Welcome to chia-tools install script"
+echo "Welcome to the chiamerge install script"
 echo ""
-echo "This will install prerequesites and copy the mnt-garden.mount to your systemd folder and reload the systemd daemon"
-echo "The rest can be found in /usr/local/bin/"
+echo "This will install all prerequesites and the systemd services to your systemd folder and reload the systemd daemon"
+echo "Everything else can then be found in /usr/local/bin/"
+echo ""
+echo "Prerequesites to be installed are: udisks2 mergerfs smartmontools and ntfs-3g"
+echo "The systemd services are called: mnt-garden.mount and mount-chia-drives.service and are located at /etc/systemd/system/"
+echo ""
+echo "No destructive actions are being perfomed upon this installation. Your plots are safe!"
 echo ""
 read -p "Continue? (y/n)" ANS
 echo ""
 sudo mkdir -p /mnt/garden
-sudo apt install -y udisks2 mergerfs smartmontools
+sudo apt install -y udisks2 mergerfs smartmontools ntfs-3g
 sudo cp -v mnt-garden.mount /etc/systemd/system/
 sudo cp -v mount-chia-drives.service /etc/systemd/system/
 sudo cp -v chia-mountall /usr/local/bin
